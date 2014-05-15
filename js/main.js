dd4883816bc01b4b1572f81f9a67b6366522e1c8
Devin Crandall <devin@weareshowpony.com>
2014-05-15 10:55:47 -0700

    Site Homepage template complete and all active files added to project.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//////////////////////////////////////////
+////////////// 	 CCTG JS    //////////////
+//////////////////////////////////////////
+
+
+$(document).ready(function(){
+	
+	$( '#nav li:has(ul)' ).doubleTapToGo();
+
+	$('header').stellar({
+  		 parallaxBackgrounds: true,
+  		 horizontalScrolling: false
+	});
+
+
+
+});
+
